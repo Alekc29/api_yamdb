2ea892642a5730d9bb5bed64a8fdee4fe8346389
ReginaMaksimenko <Regina_Maksimenko@mail.ru>
2022-11-02 22:02:27 +0700

    add в settings rest_framework и djoser

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'djoser',
     'api.apps.ApiConfig',
     'titles.apps.TitlesConfig',
     'users.apps.UsersConfig',
