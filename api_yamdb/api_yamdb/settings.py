8bde2a1f1e132f008c8dd78c22486e5ab7d58cad
ReginaMaksimenko <Regina_Maksimenko@mail.ru>
2022-11-02 07:51:07 +0700

    приложения зарегистированы в settings

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,9 +21,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
-    'titles',
-    'users',
+    'api.apps.ApiConfig',
+    'titles.apps.TitlesConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
