62f7683619e98efbcd833c905ce4dbb1b2bf794d
Александр Ширяев <alekco29@yandex.ru>
2022-11-04 20:18:41 +0300

    Переделал модель Юзера. Добавил валидатор в модель. Сделал Вьюсет для юзера.

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -40,12 +40,6 @@ class CommentSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
-    
-
-    def validate_username(self, username):
-        if username == 'me':
-            raise serializers.ValidationError('Использовать "me" нельзя.')
-        return username
 
     class Meta:
         fields = ('username',
@@ -54,4 +48,16 @@ class UserSerializer(serializers.ModelSerializer):
                   'last_name',
                   'bio',
                   'role')
+        model = User
+
+
+class NotAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('username',
+                  'email',
+                  'first_name',
+                  'last_name',
+                  'bio',
+                  'role')
+        read_only_fields = ('role',)
         model = User
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,13 +1,18 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.pagination import LimitOffsetPagination
+
 
 from users.models import User
 from .permissions import IsAdminOrReadOnly, IsAdminOnly
 from titles.models import Category, Comment, Genre, Review, Title
 from .serializers import (CategorySerializer, CommentSerializer,
                           GenreSerializer, ReviewSerializer,
-                          TitleSerializer, UserSerializer)
+                          TitleSerializer, UserSerializer,
+                          NotAdminSerializer)
 
 
 class CategoryGenreViewSet(viewsets.ModelViewSet):
@@ -43,10 +48,33 @@ class CommentViewSet(viewsets.ModelViewSet):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsAdminOnly,)
+    permission_classes = (IsAdminOnly, IsAuthenticated,)
+    lookup_field = 'username'
+    filter_backends = (SearchFilter,)
+    search_fields = ('username',)
     pagination_class = LimitOffsetPagination
 
-    def perform_create(self, serializer):
-        serializer.save()
+    @action(
+        methods=['GET', 'PATCH'],
+        detail=False,
+        permission_classes=(IsAuthenticated,),
+        url_path='me')
+    def get_current_user_info(self, request):
+        serializer = UserSerializer(request.user)
+        if request.method == 'PATCH':
+            if request.user.is_admin:
+                serializer = UserSerializer(
+                    request.user,
+                    data=request.data,
+                    partial=True)
+            else:
+                serializer = NotAdminSerializer(
+                    request.user,
+                    data=request.data,
+                    partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data)
 
-    
+   
