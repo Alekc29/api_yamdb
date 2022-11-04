3d29ab715bde2bb92d77a0a354bfaa9daea789e0
Сергей Будников <vokindub@mail.ru>
2022-11-04 12:11:21 +0300

    зарегистрированы эндпоинты в urls к Review, Comment

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,10 +1,19 @@
 from django.urls import include, path
-from .views import CategoryViewSet, GenreViewSet, TitleViewSet, UserViewSet
+from .views import (CategoryViewSet, GenreViewSet, TitleViewSet,
+                    UserViewSet, ReviewViewSet, CommentViewSet)
 from rest_framework.routers import DefaultRouter
 
 app_name = 'api'
 
 router = DefaultRouter()
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews')
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments')
 router.register("categories", CategoryViewSet, basename="categories")
 router.register("genres", GenreViewSet, basename="genres")
 router.register("titles", TitleViewSet, basename="titles")
