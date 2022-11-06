8163490f9dd8764cf2d38527203766cc99a4630a
Сергей Будников <vokindub@mail.ru>
2022-11-06 09:24:50 +0300

    поправлены ввалÐ валидатор двойс двойнным отзывомÐÑ

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -67,7 +67,7 @@ class ReviewSerializer(serializers.ModelSerializer):
     def validate(self, data):
         request = self.context['request']
         author = request.user
-        title_id = self.context['request'].kwargs.get('title_id')
+        title_id = self.context.get('view').kwargs.get('title_id')
         title = get_object_or_404(Title, pk=title_id)
         if Review.objects.filter(title=title, author=author).exists():
             raise ValidationError('Вы уже оставили отзыв')
