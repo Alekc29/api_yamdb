32d9a120b0f248193f1c4263009ae1b1277acf04
Сергей Будников <vokindub@mail.ru>
2022-11-08 11:21:43 +0300

    select_related for related fields2

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -65,7 +65,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
         title = get_object_or_404(
             Title,
             id=self.kwargs.get('title_id'))
-        return title.reviews.select_related('title').all()
+        return title.reviews.select_related('title', 'author').all()
 
     def perform_create(self, serializer):
         title = get_object_or_404(
@@ -82,7 +82,7 @@ class CommentViewSet(viewsets.ModelViewSet):
         review = get_object_or_404(
             Review,
             id=self.kwargs.get('review_id'))
-        return review.comments.select_related('review').all()
+        return review.comments.select_related('review', 'author').all()
 
     def perform_create(self, serializer):
         review = get_object_or_404(
